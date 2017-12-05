39070f6727213084d6a0a667099b29b3af8c75a0
gasimovh <33291612+gasimovh@users.noreply.github.com>
2017-12-05 15:37:12 +0100

    Add files via upload

---
+++ Class10/Handler.java
@@ -0,0 +1,16 @@
+package models;
+
+import java.util.Date;
+import java.util.ArrayList;
+
+public class Handler{
+	private Date entryDate;
+	private Date exitDate;
+	
+	Patient p = new Patient("Ozzy", "+994514806608", "01/01/1991", "A+");
+	
+	ArrayList<Doctor> doctors = new ArrayList<>();
+
+	
+	
+}
---
+++ Class10/Patient.java
@@ -0,0 +1,11 @@
+package models;
+
+import java.util.Date;
+
+public class Patient extends Person{
+	
+	public Patient(String name, String phone, String birthday, String bloodType){
+		super(name, phone, birthday, bloodType);
+	}
+	
+}
